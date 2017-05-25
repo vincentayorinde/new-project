e92f222dcfe6232bd0f3e37473287146010e2cec
vincentayorinde <vincentayorinde@yahoo.com>
2017-05-25 12:53:59 +0000

    Create hello-world.php

---
+++ hello-world.php
@@ -0,0 +1,16 @@
+<?php 
+
+class user{
+    private var $name;
+    private var $age;
+    public function display($name, $age){
+      $this-> name  = $name;
+      $this-> age   = $age;
+      return
+    }
+}
+
+$user1 = new user();
+$user-> display("Vincent", 24);
+
+?>
